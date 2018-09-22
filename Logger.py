dd9d64f38483ad3f794de1186e57a1b0847b68f5
WilliamPLamb <williamplamb@gmail.com>
2018-09-22 07:26:25 -0700

    Scaffolded Logger.py file and started testing logging

--- Logger.py
+++ Logger.py
@@ -3,91 +3,149 @@
 # 9/22/18
 # williamplamb@gmail.com
 
-import pyautogui as pag
+#######################################
+# Define imports
+
+# import pyautogui as pag
 from pynput import keyboard
-from time import sleep
+from pynput import mouse
+# from time import sleep
 
-print("Press Ctrl-X to start recording")
-print("Press Ctrl-X to stop recording")
+#######################################
+# Define functions
+
+# Mouse logger
+def on_move(x, y):
+    print('Pointer moved to {0}'.format((x, y)));
+
+def on_click(x, y, button, pressed):
+    print('{0} at {1}'.format('Pressed' if pressed else 'Released',(x, y)));
+    if not pressed:
+        # Stop listener
+        return False;
+
+def on_scroll(x, y, dx, dy):
+    print('Scrolled {0} at {1}'.format('down' if dy < 0 else 'up',(x, y)));
+
+# Keyboard logger
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(key.char));
+    except AttributeError:
+        print('special key {0} pressed'.format(key));
+
+def on_release(key):
+    print('{0} released'.format(key));
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False;
+
+
+#######################################
+# Start program
+
+# Instructions
+
+print("Press Ctrl-X to start logging")
+print("Press Ctrl-X to stop logging")
 print("Press Ctrl-V to start playback")
-print("Press Ctrl-C to quit")
+print("Press Ctrl-C to quit at any time")
+
+# Construct keyboard and mouse listeners
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as kListener,
+    mouse.Listener(
+        on_move=on_move,
+        on_click=on_click,
+        on_scroll=on_scroll) as mListener:
+    # Log with keyboard interrupt as exception
+    try:
+        print("Logging")
+        keyListener.join();
+        mouseListener.join();
+
+        # while True:
+        #     sleep(0.05);
+        #     if()
+
+    except KeyboardInterrupt:
+        print("\nExiting Program");
+        quit();
+
+# Playback
+
+#######################################
 
 
-#print("Press Ctrl-X to start and stop recording")
-#print("Press Ctrl-V to start playback")
 
-#Start
 
-try:
-    while True:
-        sleep(0.05);
-        if()
-
-except KeyboardInterrupt:
-    print("\nQuitting");
-    quit();
+#######################################
+#######################################
+#######################################
+# Old code...useful?
 
 #Record
 #class to store data
-class dataStor:
-    waitTime = 0.0f
-    x = 0
-    y = 0
-    duration = 0.0f
-    action = ""
-    button = ""
-    message = ""
-
-    def doAction(self):
-        sleep(waitTime)
-        if action == "":
-        if action == "click":
-            pag.click(x,y)
-        if action == "rightClick":
-            pag.moveTo(x,y)
-            pag.click(button='right')
-        if action == "moveTo":
-            pag.moveTo(x,y,duration, pag.easeInElastic)
-        if action == "press":
-            pag.press(button)
-        if action == "typewrite":
-            pag.typewrite(message)
-
-#array to store data
-actions[]
-
-#get position first time
-x,y = pag.position()
-action = dataStor()
-action.x = x
-action.y = y
-action = "moveTo"
-
-actions.append(action)
-
-try:
-    while True:
-        #Make timer for wait
-
-        appendActionFlag = false;
-        action = dataStor()
-        #check whether mouse has changed position
-        xNew, yNew = pag.position()
-        if x == xNew && y == yNew:
-            #position hasn't changed, don't do anything
-            action.x = x
-            action.y = y
-        else:
-            #position has changed
-            appendActionFlag = true;
-            action.x = xNew
-            action.y = yNew
-
-        #Figure out how to register what events just happened and write to action
-
-
-        sleep(0.05)
-except KeyboardInterrupt:
-    print("\nDone")
-
-#Paste actions back
+# class dataStor:
+#     waitTime = 0.0f
+#     x = 0
+#     y = 0
+#     duration = 0.0f
+#     action = ""
+#     button = ""
+#     message = ""
+#
+#     def doAction(self):
+#         sleep(waitTime)
+#         if action == "":
+#         if action == "click":
+#             pag.click(x,y)
+#         if action == "rightClick":
+#             pag.moveTo(x,y)
+#             pag.click(button='right')
+#         if action == "moveTo":
+#             pag.moveTo(x,y,duration, pag.easeInElastic)
+#         if action == "press":
+#             pag.press(button)
+#         if action == "typewrite":
+#             pag.typewrite(message)
+#
+# #array to store data
+# actions[]
+#
+# #get position first time
+# x,y = pag.position()
+# action = dataStor()
+# action.x = x
+# action.y = y
+# action = "moveTo"
+#
+# actions.append(action)
+#
+# try:
+#     while True:
+#         #Make timer for wait
+#
+#         appendActionFlag = false;
+#         action = dataStor()
+#         #check whether mouse has changed position
+#         xNew, yNew = pag.position()
+#         if x == xNew && y == yNew:
+#             #position hasn't changed, don't do anything
+#             action.x = x
+#             action.y = y
+#         else:
+#             #position has changed
+#             appendActionFlag = true;
+#             action.x = xNew
+#             action.y = yNew
+#
+#         #Figure out how to register what events just happened and write to action
+#
+#
+#         sleep(0.05)
+# except KeyboardInterrupt:
+#     print("\nDone")
+#
+# #Paste actions back
