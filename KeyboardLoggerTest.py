5e5d424a8065dfa65dd5ad7ea552fa29019bdfdd
WilliamPLamb <williamplamb@gmail.com>
2018-09-22 06:40:03 -0700

    Added test file for mouse logging

--- KeyboardLoggerTest.py
+++ KeyboardLoggerTest.py
@@ -3,19 +3,18 @@
 # 9/22/18
 # williamplamb@gmail.com
 
-#From pynput docs: https://pynput.readthedocs.io/en/latest/keyboard.html#monitoring-the-keyboard
+# From pynput docs: https://pynput.readthedocs.io/en/latest/keyboard.html#monitoring-the-keyboard
+# More info: https://theembeddedlab.com/tutorials/keylogger-python/
 
 from pynput import keyboard
 
-print("Starting Program");
+# Function Definitions
 
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        print('alphanumeric key {0} pressed'.format(key.char))
     except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        print('special key {0} pressed'.format(key))
 
 def on_release(key):
     print('{0} released'.format(
@@ -24,8 +23,14 @@ def on_release(key):
         # Stop listener
         return False
 
+
+
+# Start program
+print("Starting Program");
+
 # Collect events until released
 with keyboard.Listener(
+        on_move=on_move,
         on_press=on_press,
         on_release=on_release) as listener:
     try:
---
+++ MouseLoggerTest.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+# William Lamb
+# 9/22/18
+# williamplamb@gmail.com
+
+# From pynput docs: https://pynput.readthedocs.io/en/latest/keyboard.html#monitoring-the-keyboard
+
+from pynput import mouse
+
+# Function Definitions
+
+def on_move(x, y):
+    print('Pointer moved to {0}'.format((x, y)));
+
+def on_click(x, y, button, pressed):
+    print('{0} at {1}'.format('Pressed' if pressed else 'Released',(x, y)));
+    if not pressed:
+        # Stop listener
+        return False;
+
+def on_scroll(x, y, dx, dy):
+    print('Scrolled {0} at {1}'.format('down' if dy < 0 else 'up',(x, y)));
+
+# Collect events until released
+with mouse.Listener(
+        on_move=on_move,
+        on_click=on_click,
+        on_scroll=on_scroll) as listener:
+    try:
+        print("Starting program")
+        listener.join();
+    except KeyboardInterrupt:
+        print("\nExiting Program");
+        quit();
